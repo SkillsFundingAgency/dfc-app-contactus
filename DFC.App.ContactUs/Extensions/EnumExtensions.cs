8c7c16e8b669759524ade393c23f377892d0508a
Ian Crisp <icrisp@program-systems.co.uk>
2020-05-13 14:53:38 +0100

    Addressed SonarCloud concerns

--- DFC.App.ContactUs/Extensions/EnumExtensions.cs
+++ DFC.App.ContactUs/Extensions/EnumExtensions.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Reflection;
 
 namespace DFC.App.ContactUs.Extensions
 {
+    [ExcludeFromCodeCoverage]
     public static class EnumExtensions
     {
         public static string GetDescription<T>(this T e)
