bdbd72b9947f825d26d0a74c41e8bcd6307f7e8f
Ian Crisp <icrisp@program-systems.co.uk>
2020-05-13 14:46:12 +0100

    Removed obsolete method

--- DFC.App.ContactUs/Extensions/EnumExtensions.cs
+++ DFC.App.ContactUs/Extensions/EnumExtensions.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.ComponentModel;
-using System.Globalization;
 using System.Linq;
 using System.Reflection;
 
@@ -8,39 +6,6 @@ namespace DFC.App.ContactUs.Extensions
 {
     public static class EnumExtensions
     {
-        public static string GetDescriptionX<T>(this T e)
-            where T : IConvertible
-        {
-            if (e is Enum)
-            {
-                Type type = e.GetType();
-                var values = (int[])System.Enum.GetValues(type);
-
-                foreach (int val in values)
-                {
-                    if (val == e.ToInt32(CultureInfo.InvariantCulture))
-                    {
-                        var enumName = type.GetEnumName(val);
-
-                        if (!string.IsNullOrWhiteSpace(enumName))
-                        {
-                            var memInfo = type.GetMember(enumName);
-                            var descriptionAttribute = memInfo[0]
-                                .GetCustomAttributes(typeof(DescriptionAttribute), false)
-                                .FirstOrDefault() as DescriptionAttribute;
-
-                            if (descriptionAttribute != null)
-                            {
-                                return descriptionAttribute.Description;
-                            }
-                        }
-                    }
-                }
-            }
-
-            return string.Empty;
-        }
-
         public static string GetDescription<T>(this T e)
             where T : IConvertible
         {
