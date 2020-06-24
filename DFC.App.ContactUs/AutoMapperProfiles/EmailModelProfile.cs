dc43ffaf04deb24794a181541640575bd051ebf4
Tom Tollafield <thomas.tollafield@gmail.com>
2020-06-24 10:19:16 +0100

    - Remove unncessesary map

--- DFC.App.ContactUs/AutoMapperProfiles/EmailModelProfile.cs
+++ DFC.App.ContactUs/AutoMapperProfiles/EmailModelProfile.cs
@@ -13,7 +13,6 @@ namespace DFC.App.ContactUs.AutoMapperProfiles
         {
             CreateMap<EmailApiDataModel, EmailModel>()
                .ForMember(d => d.Id, s => s.MapFrom(a => Guid.Parse(a.Url!.Segments.Last())))
-               .ForMember(d => d.Body, s => s.MapFrom(a => a.Body))
                .ForMember(d => d.Etag, s => s.Ignore())
                .ForMember(d => d.ParentId, s => s.Ignore())
                .ForMember(d => d.TraceId, s => s.Ignore())
