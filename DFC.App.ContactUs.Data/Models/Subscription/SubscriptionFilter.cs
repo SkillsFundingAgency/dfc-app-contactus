56354b710d7edaaab058257e500f62de9dad227e
Tom Tollafield <thomas.tollafield@gmail.com>
2020-07-03 14:03:09 +0100

    Exclude from code coverage

--- DFC.App.ContactUs.Data/Models/Subscription/SubscriptionFilter.cs
+++ DFC.App.ContactUs.Data/Models/Subscription/SubscriptionFilter.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 
 namespace DFC.App.ContactUs.Data.Models.Subscription
 {
+    [ExcludeFromCodeCoverage]
     public class SubscriptionFilter
     {
         public List<SubscriptionPropertyContainsFilter>? PropertyContainsFilters { get; set; }
