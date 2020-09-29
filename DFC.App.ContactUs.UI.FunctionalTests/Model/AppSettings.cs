a5c1b8b2709b9c8ddad5e6f547b338f5be3d428b
James Barnes <james@testtrends.co.uk>
2020-09-29 13:05:13 +0100

    Included appsettings class

---
+++ DFC.App.ContactUs.UI.FunctionalTests/Model/AppSettings.cs
@@ -0,0 +1,24 @@
+﻿// <copyright file="AppSettings.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+using DFC.TestAutomation.UI.Config;
+using DFC.TestAutomation.UI.Helpers;
+
+namespace DFC.App.ContactUs.Model
+{
+    internal class AppSettings : IConfiguration
+    {
+        public ProjectConfig ProjectConfig { get; set; }
+
+        public TimeOutConfig TimeOutConfig { get; set; }
+
+        public BrowserStackSetting BrowserStackConfig { get; set; }
+
+        public EnvironmentConfig EnvironmentConfig { get; set; }
+
+        public TestExecutionConfig TestExecutionConfig { get; set; }
+
+        public MongoDbConfig MongoDbConfig { get; set; }
+    }
+}
